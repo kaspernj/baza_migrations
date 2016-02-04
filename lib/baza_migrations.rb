583ba72792bcda22ceb3cc36b9e6911868e8e23d
kaspernj <k@spernj.org>
2016-02-04 15:47:05 +0100

    Added method to load tasks instead, because the current solution didn't really work

--- lib/baza_migrations.rb
+++ lib/baza_migrations.rb
@@ -1,8 +1,10 @@
 require "baza"
 require "auto_autoloader"
 
-load "#{File.dirname(__FILE__)}/tasks/baza.rake" if ::Kernel.const_defined?(:Rake)
-
 class BazaMigrations
   AutoAutoloader.autoload_sub_classes(self, __FILE__)
+
+  def self.load_tasks
+    load "#{File.dirname(__FILE__)}/tasks/baza.rake"
+  end
 end
