cbdd371403eab77813e4c537d74304c0df4bc40b
kaspernj <kaspernj@gmail.com>
2020-12-10 20:16:58 +0100

    Fixed Lint/EmptyBlock offence

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,3 @@ end
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
-
-RSpec.configure do |config|
-end
